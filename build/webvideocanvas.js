799b7c365337afe45382f65d4751fe3694b00593
Techming <Techming@users.noreply.github.com>
2022-11-22 22:47:00 +0000

    Update Build

--- build/webvideocanvas.js
+++ build/webvideocanvas.js
@@ -2845,8 +2845,8 @@ class MultiStreamViewer extends (eventemitter2_default()) {
    * @param {string} options.host - the hostname of the MJPEG server
    * @param {number} [options.port] (optional) - the port to connect to
    * @param {number} [options.quality] (optional) - the quality of the stream (from 1-100)
-   * @param {[string]} options.topics - an array of topics to stream
-   * @param {[string]} [options.labels] (optional) - an array of labels associated with each topic
+   * @param {Array<string>} options.topics - an array of topics to stream
+   * @param {Array<string>} [options.labels] (optional) - an array of labels associated with each topic
    * @param {number} [options.defaultStream] (optional) - the index of the default stream to use
    */
   constructor(options) {
